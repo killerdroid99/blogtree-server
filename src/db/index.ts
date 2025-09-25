f207edb887732fdf96c992ceb8f725e543972d9d
Aniket Jena <wiiwii2114@gmail.com>
2025-09-25 15:23:40 +0530

    fix: swithced back to neon

--- src/db/index.ts
+++ src/db/index.ts
@@ -1,5 +1,5 @@
-// import { drizzle } from 'drizzle-orm/neon-http';
-import { drizzle } from 'drizzle-orm/node-postgres';
+import { drizzle } from 'drizzle-orm/neon-http';
+// import { drizzle } from 'drizzle-orm/node-postgres';
 
 import * as schema from './schema';
 
